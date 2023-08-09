022f1963772cb4ba662babd96aad808d4c47a2f7
dhruvishabhingradiya <dhruvishabhingradiya@gmail.com>
2023-08-09 14:59:26 +0600

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
   app.use(cors());
   app.use(express.urlencoded());
 
-  const dbURI = process.env.MONGODB_URI;
+  const dbURI = 'mongodb+srv://dhruvishabhingradiya:4hIvZuqpZjh7ZrpL@users.lrfpqqz.mongodb.net/?retryWrites=true&w=majority';
   
   mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
